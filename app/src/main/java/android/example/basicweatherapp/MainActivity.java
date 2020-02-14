5d4ac2e8c78c4837b0a4e92eaebada0c3850e1f9
Sinc0 <sinco.developer@gmail.com>
2020-02-14 01:29:19 +0100

    String handling, conversions, api connection + first letter of status fixed to upper + conversions from kelvin to celsius + celsius symbol ° added aka degree symbol + api connected + info text changes correctly + comments + formatting

--- app/src/main/java/android/example/basicweatherapp/MainActivity.java
+++ app/src/main/java/android/example/basicweatherapp/MainActivity.java
@@ -7,26 +7,46 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonWeatherInfo;
+
     EditText editTextEnterWeatherLocation;
+
+    TextView textViewDisplayWeatherInfo;
 
     String editTextString;
     String downloadUrl;
     String weatherLocation;
     String ak = "&appid=f394e4fb836c1332f30df5d91d30d9ab";
     String temperature;
-    String humidity;
     String feelsLike;
+    String humidity;
+    String airFeels = "asdf";
+    String description;
+    String celsius;
+
+    int humidityInt = 0;
+    int celsiusInt = 0;
+    int feelsLikeInt = 0;
+
+    double temperatureDouble = 0;
+    double feelsLikeDouble = 0;
+
+    Toast toastCityNotFound;
+
+    DecimalFormat decimals2;
 
     public void DownloadWeatherData()
     {
@@ -96,28 +116,79 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONArray arrayWeatherInfo = new JSONArray(weatherInfo);
 
+                //sort JSON weather content
                 for (int i = 0; i < arrayWeatherInfo.length(); i++)
                 {
                     JSONObject weatherHolder = arrayWeatherInfo.getJSONObject(i);
 
-                    Log.i("Description", weatherHolder.getString("description"));
+                    description = weatherHolder.getString("description");
+
+                    description = description.substring(0,1).toUpperCase() + description.substring(1).toLowerCase();
+
                 }
 
+                //sort JSON main content
                 JSONObject mainHolder = jsonObject.getJSONObject("main");
                 temperature = mainHolder.getString("temp");
                 feelsLike = mainHolder.getString("feels_like");
                 humidity = mainHolder.getString("humidity");
 
+                //convert temperatures from string to double
+                temperatureDouble = Double.parseDouble(temperature);
+                feelsLikeDouble = Double.parseDouble(feelsLike);
 
-                Log.i("Temperature", temperature);
-                Log.i("Feels Like", feelsLike);
-                Log.i("Humidity", humidity);
+                //round up numbers
+                temperatureDouble = Math.round(temperatureDouble);
+                feelsLikeDouble = Math.round(feelsLikeDouble);
+
+                //convert from kelvin to celsius
+                temperatureDouble = temperatureDouble - 273;
+                feelsLikeDouble = feelsLikeDouble - 273;
+
+                //convert temperatures from double to int
+                celsiusInt = (int) temperatureDouble;
+                feelsLikeInt = (int) feelsLikeDouble;
+
+                //convert temperatures from int to string
+                humidityInt = Integer.parseInt(humidity);
+                celsius = Integer.toString(celsiusInt);
+                feelsLike = Integer.toString(feelsLikeInt);
+
+                if (humidityInt <= 20)
+                {
+                    airFeels = "Dry";
+                }
+
+                if (humidityInt >= 20 && humidityInt <= 60)
+                {
+                    airFeels = "Normal";
+                }
+
+                if (humidityInt >= 60 && humidityInt > 60)
+                {
+                    airFeels =  "Wet";
+                }
+
+                textViewDisplayWeatherInfo.setText
+                (
+                    "Status: " + description + "\n" +
+                    "Temperature: " + celsius + "°" + "\n" +
+                    "Feels like: " + feelsLike + "°" + "\n" +
+                    "Humidity: " + humidity + "\n" +
+                    "Air: " + airFeels
+                );
 
             }
 
             catch (Exception e)
             {
                 e.printStackTrace();
+
+                Log.i("Tag", "City not found");
+
+                toastCityNotFound.show();
+
+                textViewDisplayWeatherInfo.setText("");
             }
         }
     }
@@ -129,6 +200,9 @@ public class MainActivity extends AppCompatActivity {
 
         buttonWeatherInfo = findViewById(R.id.buttonWeatherInfo);
         editTextEnterWeatherLocation = findViewById(R.id.editTextEnterWeatherLocation);
+        textViewDisplayWeatherInfo = findViewById(R.id.textViewDisplayWeatherInfo);
+
+        toastCityNotFound = Toast.makeText(getApplicationContext(), "City not found", Toast.LENGTH_SHORT);
 
     }
 }
