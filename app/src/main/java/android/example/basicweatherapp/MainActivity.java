3533d29a5549f75444467ab90eeb2dfe628c3584
Sinc0 <sinco.developer@protonmail.com>
2023-07-27 17:41:54 +0200

    minor structural improvements

--- app/src/main/java/android/example/basicweatherapp/MainActivity.java
+++ app/src/main/java/android/example/basicweatherapp/MainActivity.java
@@ -1,5 +1,7 @@
+//name
 package android.example.basicweatherapp;
 
+//includes
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -17,14 +19,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.lang.Math;
 
+//main
 public class MainActivity extends AppCompatActivity {
 
+    //widgets
     Button buttonWeatherInfo;
-
     EditText editTextEnterWeatherLocation;
-
     TextView textViewDisplayWeatherInfo;
+    Toast toastCityNotFound;
 
+    //variables
     String editTextString;
     String downloadUrl;
     String weatherLocation;
@@ -36,16 +40,13 @@ public class MainActivity extends AppCompatActivity {
     String description;
     String celsius;
     String country;
-
     int humidityInt = 0;
     int celsiusInt = 0;
     int feelsLikeInt = 0;
-
     double temperatureDouble = 0;
     double feelsLikeDouble = 0;
 
-    Toast toastCityNotFound;
-
+    //functions
     public void DownloadWeatherData()
     {
         DownloadTask downloadTask = new DownloadTask();
@@ -54,27 +55,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickWeatherInfo(View v)
     {
-
+        //variables
         editTextString = editTextEnterWeatherLocation.getText().toString();
-
         weatherLocation = editTextString;
-
         downloadUrl = "https://api.openweathermap.org/data/2.5?q/weather=" + weatherLocation + ak;
 
         DownloadWeatherData();
 
+        //log
         Log.i("Tag", "Weather Info Button Pressed");
     }
 
+    //classes
     public class DownloadTask extends AsyncTask<String, Void, String>
     {
         @Override
         protected String doInBackground(String... urls)
         {
+            //variables
             String result = "";
             URL url;
+
+            //set http url connection
             HttpURLConnection urlConnection = null;
 
+            //try download
             try
             {
                 url = new URL(urls[0]);
@@ -92,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
                 return result;
             }
-
             catch (Exception e)
             {
                 e.printStackTrace();
@@ -105,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
+            //try parse data
             try
             {
                 JSONObject jsonObject = new JSONObject(s);
@@ -129,70 +134,43 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-                //sort JSON main content
+                //sort JSON content
                 JSONObject mainHolder = jsonObject.getJSONObject("main");
                 temperature = mainHolder.getString("temp");
                 feelsLike = mainHolder.getString("feels_like");
                 humidity = mainHolder.getString("humidity");
 
-                //convert temperatures from string to double
-                temperatureDouble = Double.parseDouble(temperature);
-                feelsLikeDouble = Double.parseDouble(feelsLike);
+                //convert temperatures
+                temperatureDouble = Double.parseDouble(temperature); //from string to double
+                feelsLikeDouble = Double.parseDouble(feelsLike); //from string to double
+                temperatureDouble = Math.round(temperatureDouble); ////round up numbers
+                feelsLikeDouble = Math.round(feelsLikeDouble); ////round up numbers
+                temperatureDouble = temperatureDouble - 273; //from kelvin to celsius
+                feelsLikeDouble = feelsLikeDouble - 273; //from kelvin to celsius
+                celsiusInt = (int) temperatureDouble; //from double to int
+                feelsLikeInt = (int) feelsLikeDouble; //from double to int
+                humidityInt = Integer.parseInt(humidity); //from int to string
+                celsius = Integer.toString(celsiusInt); //from int to string
+                feelsLike = Integer.toString(feelsLikeInt); //from int to string
 
-                //round up numbers
-                temperatureDouble = Math.round(temperatureDouble);
-                feelsLikeDouble = Math.round(feelsLikeDouble);
+                //set airFeels
+                if (humidityInt <= 20) { airFeels = "Dry"; }
+                if (humidityInt >= 20 && humidityInt <= 60) { airFeels = "Normal"; }
+                if (humidityInt >= 60 && humidityInt > 60) { airFeels =  "Wet"; }
 
-                //convert from kelvin to celsius
-                temperatureDouble = temperatureDouble - 273;
-                feelsLikeDouble = feelsLikeDouble - 273;
+                //sets image background based on celsius number
+                if (celsiusInt < 10) { getWindow().setBackgroundDrawableResource(R.drawable.cold); }
+                if (celsiusInt > 10 && celsiusInt <= 20) { getWindow().setBackgroundDrawableResource(R.drawable.normal); }
+                if (celsiusInt > 20) { getWindow().setBackgroundDrawableResource(R.drawable.sun); }
 
-                //convert temperatures from double to int
-                celsiusInt = (int) temperatureDouble;
-                feelsLikeInt = (int) feelsLikeDouble;
-
-                //convert temperatures from int to string
-                humidityInt = Integer.parseInt(humidity);
-                celsius = Integer.toString(celsiusInt);
-                feelsLike = Integer.toString(feelsLikeInt);
-
-                //sets air feel based on humidity number
-                if (humidityInt <= 20)
-                {
-                    airFeels = "Dry";
-                }
-
-                if (humidityInt >= 20 && humidityInt <= 60)
-                {
-                    airFeels = "Normal";
-                }
-
-                if (humidityInt >= 60 && humidityInt > 60)
-                {
-                    airFeels =  "Wet";
-                }
-
-                //sets background based on celsius number
-                if (celsiusInt < 10)
-                {
-                    getWindow().setBackgroundDrawableResource(R.drawable.cold);
-                }
-
-                if (celsiusInt > 10 && celsiusInt <= 20)
-                {
-                    getWindow().setBackgroundDrawableResource(R.drawable.normal);
-                }
-
-                if (celsiusInt > 20)
-                {
-                    getWindow().setBackgroundDrawableResource(R.drawable.sun);
-                }
-
+                //set country
                 JSONObject sysHolder = jsonObject.getJSONObject("sys");
                 country = sysHolder.getString("country");
 
+                //display weather info textview
                 textViewDisplayWeatherInfo.setVisibility(View.VISIBLE);
 
+                //update weather info text
                 textViewDisplayWeatherInfo.setText
                 (
                     "Temperature: " + celsius + "°" + "\n" +
@@ -204,15 +182,11 @@ public class MainActivity extends AppCompatActivity {
                 );
 
             }
-
             catch (Exception e)
             {
                 e.printStackTrace();
-
                 Log.i("Tag", "City not found");
-
                 toastCityNotFound.show();
-
                 textViewDisplayWeatherInfo.setText("");
             }
         }
@@ -221,15 +195,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         getWindow().setBackgroundDrawableResource(R.drawable.mainbackground);
 
         buttonWeatherInfo = findViewById(R.id.buttonWeatherInfo);
         editTextEnterWeatherLocation = findViewById(R.id.editTextEnterWeatherLocation);
         textViewDisplayWeatherInfo = findViewById(R.id.textViewDisplayWeatherInfo);
 
+        //error message
         toastCityNotFound = Toast.makeText(getApplicationContext(), "City not found", Toast.LENGTH_SHORT);
-
     }
 }
